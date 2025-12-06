a002d03fbe46652e8e8af42961cdd91517178ce6
Dave <bartolabacdave710@gmail.com>
2025-12-06 22:31:36 +0800

    added error msg

--- src/main/java/com/inventory/FilterOrder.java
+++ src/main/java/com/inventory/FilterOrder.java
@@ -3,6 +3,7 @@ package com.inventory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class FilterOrder {
@@ -15,6 +16,9 @@ public class FilterOrder {
 
     @FXML
     private TextField enterProd;
+
+    @FXML
+    private Label errMsg;
 
     @FXML
     private Button filterButton;
